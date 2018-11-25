6ae90ceaabc7712b010de0bf19ae883b1b78d08f
mom0tomo <momo76w@gmail.com>
2018-11-25 16:16:39 +0900

    fix

--- keys/keys.go
+++ keys/keys.go
@@ -7,10 +7,10 @@ import (
 )
 
 // GetTwitterAPI is getting the Twitter API's responce
-func GetTwitterAPI() (*anaconda.TwitterApi, error) {
+func GetTwitterAPI() *anaconda.TwitterApi {
 
 	anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
 	api := anaconda.NewTwitterApi(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
-	return api, nil
+	return api
 }
